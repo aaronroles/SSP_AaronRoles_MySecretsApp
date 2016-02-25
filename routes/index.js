552302815d2f0684b03604e7d65f6fb4e6d3ae3e
Aaron Roles <aaron_roles93@hotmail.com>
2016-02-25 20:29:35 +0000

    Updated link to secrets page

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,17 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Log In' });
+  console.log("HELLO 1");
 });
 
 router.get('/index', function(req, res, next){
-  console.log("HELLO");
+  res.render('index', { title: 'Log In' });
+  console.log("HELLO 2");
 });
+
+router.post('/secrets', function(req, res, next){
+  res.render('secrets', { title: 'My Secrets App' });
+  console.log("HELLO 3");
+});
+
 module.exports = router;
